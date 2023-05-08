47d2ecb8f01d3cceb4adfccbc18169b5aceb87b2
riyad3399 <programminghero.riyad@gmail.com>
2023-05-08 16:24:15 +0600

    simple-crud-server complied

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// vhHjXdQ4jiJGA62L
+
+const uri =
+  "mongodb+srv://programmingheroriyad:vhHjXdQ4jiJGA62L@cluster0.xiw11k9.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    app.get("/users", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const user = await userCollection.findOne(query)
+      res.send(user)
+    })
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user);
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updatedUser = {
+        $set: {
+          name: user.name, 
+          email: user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser, options)
+      res.send(result)
+    })
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new user", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async(req, res) => {
+      const id = req.params.id;
+        console.log("please delete from database", id);
+        const query = { _id: new ObjectId(id) }
+        const result = await userCollection.deleteOne(query)
+        res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("simple curd server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
